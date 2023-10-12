b995bb5c8e14bd866839a280e24db755dfc1f11d
Andy Damevin <ia3andy@gmail.com>
2023-10-12 17:29:09 +0200

    Fix bundling

--- src/main/resources/web/app/js/script.js
+++ src/main/resources/web/app/js/script.js
@@ -1,3 +1,27 @@
-import "jquery"
+import $ from "jquery"
 import "patternfly/dist/css/patternfly.css";
-import "patternfly/dist/css/patternfly.css";
+import "patternfly/dist/css/patternfly-additions.css";
+import "datatables.net";
+import "drmonty-datatables-colvis";
+import "datatables.net-colreorder";
+import "bootstrap-switch";
+import "jquery-match-height";
+
+window.jQuery = $;
+
+require("bootstrap");
+require("patternfly/dist/js/patternfly.js");
+require("bootstrap-touchspin")
+
+$(document).ready(function () {
+
+    // matchHeight the contents of each .card-pf and then the .card-pf itself
+    $(".row-cards-pf > [class*='col'] > .card-pf .card-pf-title").matchHeight();
+    $(".row-cards-pf > [class*='col'] > .card-pf > .card-pf-body").matchHeight();
+    $(".row-cards-pf > [class*='col'] > .card-pf > .card-pf-footer").matchHeight();
+    $(".row-cards-pf > [class*='col'] > .card-pf").matchHeight();
+
+    // Initialize the vertical navigation
+    $().setupVerticalNavigation(true);
+});
+
